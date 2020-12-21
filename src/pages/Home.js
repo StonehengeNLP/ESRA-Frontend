ad7f1f4716b46ec0c1405491ed5af32632be4339
Peeranuth Kehasukcharoen <peeranuth.k@gmail.com>
2020-12-22 02:37:31 +0700

    Change: rearrange home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,28 @@
 import React from 'react'
 import Searchbar from '../components/Searchbar'
-import {Container, Row, Col} from 'react-bootstrap'
+import {Container, Row, Col, Button} from 'react-bootstrap'
 import '../css/home.css'
 
 
 function home() {
     return (
-        // <div className="h-100 align-items-center">
-        //     <Container className="h-100 align-items-center justify-content-center" >
-        //         <Row className='align-items-center h-100'>
-        //             <Col md={4}>md=4</Col>
-        //             <Col md={{ span: 4, offset: 4 }}>{`md={{ span: 4, offset: 4 }}`}</Col>
-        //         </Row>
-        //     </Container>
-        // </div>
-        <div className="h-100 align-items-center">
+        <div className='h-100'>
+            <div className="home-search align-items-center">
                 <Container className="h-100 align-items-center">
-                    <Row className="h-75 align-items-center">
+                    <Row className="h-100 align-items-center">
                         <Col xs md>
                             <h1>ESRA</h1>
                             <h4>Explanable Scientific Research Assistant</h4>
                             <Searchbar />
                         </Col>
                     </Row>
-            </Container>
+                </Container>
+            </div>
+            <footer className="home-footer">
+            </footer>
         </div>
+        
+        
     )
 }
 
