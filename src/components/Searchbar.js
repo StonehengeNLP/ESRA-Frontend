61888c4738c023149cd33673d8582281d6daf972
Peeranuth Kehasukcharoen <peeranuth.k@gmail.com>
2020-12-22 16:41:32 +0700

    Change: searchbar hook

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,32 +1,31 @@
-import React, { Component } from 'react'
+import React, {useState, useEffect} from 'react'
 
 import '../css/searchbar.css'
 
-export default class Searchbar extends Component {
+function Searchbar() {
+    
+    const [keyword, setKeyword] = useState("")
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            search : "",
-
-        }
+    const onClickSearch = () => {
+        console.log(keyword);
     }
 
-    render() {
-        return (
-            <form className="search-bar">
+    return (
+        <form className="search-bar">
                 <input
                 id="search"
                 type="text"
                 placeholder="Search any topics"
+                onChange={(e)=>setKeyword(e.target.value)}
                 ></input>
-                <div className="search-button" role="button">
+                <div className="search-button" role="button" onClick={onClickSearch}>
                     <svg focusable="false" xmlns="http://www.w3.org/2000/svg" width="7vh" height="7vh">
                         <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z">
                         </path>
                     </svg>
                 </div>
-            </form>
-        )
-    }
+        </form>
+    )
 }
+
+export default Searchbar
