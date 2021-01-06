9ec61f0c1a0554cbdab01762dc312925da30b18d
Peeranuth Kehasukcharoen <peeranuth.k@gmail.com>
2021-01-07 00:20:30 +0700

    Add: init fetch cited/cite information

--- src/pages/Paper.js
+++ src/pages/Paper.js
@@ -5,9 +5,12 @@ import GraphVis from '../components/GraphVis';
 import '../css/paper.css';
 import axios from 'axios';
 import useQuery from '../functions.js';
+import PaperList from '../components/PaperList';
+import PaperItem from '../components/PaperItem';
+import { Tabs, Tab } from 'react-bootstrap';
 
 const testPaper =  {
-    'title': 'DoubleBERT: pretrained BERT for Dharma understanding',
+    'paper_title': 'DoubleBERT: pretrained BERT for Dharma understanding',
     'authors': ['Red Sensei', 'H. Sasada'],
     'affiliations': 'Gang of Four',
     'conference': 'GoF Con 2021',
@@ -17,19 +20,44 @@ const testPaper =  {
 }
 
 const backendPaperUrl = 'http://localhost:8000/api/paper/get_paper'
+const backendPaperList = 'http://localhost:8000/api/paper/paper_list'
+
 
 function Paper(props) {
     let params = useQuery();
     const [paper, setPaper] = useState([]);
+    const [refs, setRefs] = useState([]);
+    const [cited, setCited] = useState([]);
     const paper_id = props.match.params.id; 
 
     useEffect(() => {
+        let c = null;
         axios.get(backendPaperUrl, {
             params: {
                 paper_id: paper_id,
             },
         }).then(res => {
             setPaper(res.data);
+            document.title = res.data.paper_title;
+            c = res.data.cited_by;
+            let refIds = res.data.cite_to.join(',');
+            return axios.get(backendPaperList, {
+                params: {
+                    paper_ids: refIds,
+                    no_ex: true
+                }
+            })
+        }).then(res => {
+            setRefs(res.data);
+            let cited_ids = c.slice(0,10).join(',');
+            return axios.get(backendPaperList, {
+                params: {
+                    paper_ids: cited_ids,
+                    no_ex: true
+                }
+            })
+        }).then(res => {
+            setCited(res.data);
         });
     }, []);
 
@@ -47,7 +75,23 @@ function Paper(props) {
                 <p className='abstract'>{paper.abstract}</p>
             </div>
             <GraphVis />
-            
+            <br></br>
+            <div className='paper-info-container'>
+                <div className='cite-tab'>
+                    <Tabs defaultActiveKey='reference'>
+                        <Tab eventKey='reference' title='Reference'>
+                            <PaperList>
+                                {refs.map(paper => (<PaperItem key={paper.paper_id} paper={paper}></PaperItem>))}
+                            </PaperList>
+                        </Tab>
+                        <Tab eventKey='cited_by' title='Cited by'>
+                            <PaperList>
+                                {cited.map(paper => (<PaperItem key={paper.paper_id} paper={paper}></PaperItem>))}
+                            </PaperList>
+                        </Tab>
+                    </Tabs>
+                </div>
+            </div>
             <br></br>
             <footer></footer>
         </Fragment>
