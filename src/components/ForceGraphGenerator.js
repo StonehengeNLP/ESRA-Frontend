bae39f4606a77abca2eb49285c1be9f02cdc0c3e
Peeranuth Kehasukcharoen <peeranuth.k@gmail.com>
2021-03-03 02:33:31 +0700

    Change: force parameter

--- src/components/ForceGraphGenerator.js
+++ src/components/ForceGraphGenerator.js
@@ -192,8 +192,8 @@ export default function runForceGraph(container, linksData, nodesData, nodesHove
 
     const simulation = d3
         .forceSimulation(nodes)
-        .force('link', d3.forceLink(links).id(d => d.id).distance(110))
-        .force('charge', d3.forceManyBody().strength(-500).distanceMax(200))
+        .force('link', d3.forceLink(links).id(d => d.id).distance(130))
+        .force('charge', d3.forceManyBody().strength(-800).distanceMax(170))
         .force("center", d3.forceCenter().x(width * .5).y(height * .5))
         .force("collide",d3.forceCollide().radius(d => d.r * 2));
     
